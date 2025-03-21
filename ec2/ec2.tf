85fd8ee6410990c90b4a4e9eb86b89044703e20b
bettari1993 <rajubettari1993@gmail.com>
2025-03-22 02:18:09 +0530

    creating instance

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -16,7 +16,17 @@ resource "aws_security_group" "allow_ssh_terraform" {
         ipv6_cidr_blocks = ["::/0"]
     }
     tags = {
-      name = "allow_ssh"
+      Name = "allow_ssh"
+    }
+  
+}
+
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terrafom"
     }
   
 }

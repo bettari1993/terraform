2d60153f81462b541aa597ea0324988fbfd2c46a
bettari1993 <rajubettari1993@gmail.com>
2025-03-22 02:07:06 +0530

    modified file

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
         from_port = 22
         to_port   = 22
         protocol  = "tcp"
-        cidr_blocks = ["0.0.0.0./0"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
     tags = {

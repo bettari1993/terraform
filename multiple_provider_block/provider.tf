1e2f1c4b08e53d99baf4f10e59762f2bdc2c7311
bettari1993 <rajubettari1993@gmail.com>
2025-03-28 16:06:54 +0530

    added multiple_provider_block

---
+++ multiple_provider_block/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92.0"
+    }
+  }
+}
+
+# Configure the AWS Provider1
+provider "aws" {
+  profile = "terraform"
+  region = "us-east-1"
+}
+
+#configure the AWS provider2
+provider "aws" {
+    profile = "terrform"
+    region = "us-west-1"
+    alias = "aws-west-1"
+  
+}
